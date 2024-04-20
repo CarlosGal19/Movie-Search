cdcca96fd5c329dc0f23e6626ecc672c97377803
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-20 14:46:17 -0600

    feat: add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [title, setTitle] = useState('Avengers');
+
+  let id = "4287ad07";
+
+  useEffect(() => {
+    console.log('App mounted');
+    fetch(`http://www.omdbapi.com/?apikey=${id}&s=${title}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, [title]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('Search button clicked');
+    setTitle('Star Wars');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='page'>
+      <header>
+        <h1>Movie Search</h1>
+        <form>
+          <input type="text" placeholder='Avengers, Star Wars, Interestellar, ...' />
+          {/* The last button of a form, is type submit */}
+          <button onClick={handleClick}>Search</button>
+        </form>
+      </header>
+      <main>
+        There will be the results
+      </main>
+    </div>
   )
 }
 
