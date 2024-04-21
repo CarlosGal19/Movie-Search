56433481764d608605266eac87b8473d60723685
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-21 17:05:21 -0600

    fix: show message only if the search is not the first one

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,16 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function useSearch(){
 
     const [title, setTitle] = useState('');
     const [error, setError] = useState(null);
+    const isFirstRun = useRef(true);
 
     useEffect(() => {
-      if(title === ''){
-        setError('Please enter a movie title');
-        return;
-      }
-      setError(null)
+        if(isFirstRun.current){
+            isFirstRun.current = title === '';
+            return;
+        }
+        if(title === ''){
+            setError('Please enter a movie title');
+            return;
+        }
+        setError(null)
     }, [title]);
 
     return {title, setTitle, error}
