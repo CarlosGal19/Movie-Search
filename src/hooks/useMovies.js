f84f01bca1c7af178ab5df31cde576ae6ed09fd6
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-21 18:06:16 -0600

    feat: avoid the same search twice

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { searchMovies } from '../services/movies';
 
 export default function useMovies({ title }) {
@@ -6,11 +6,14 @@ export default function useMovies({ title }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const previousSearch = useRef(title);
 
   const getMovies = async () => {
+    if(previousSearch.current === title) return;
     try {
       setLoading(true);
       setError(null);
+      previousSearch.current = title;
       const movies = await searchMovies(title);
       setMovies(movies);
     } catch (error) {
