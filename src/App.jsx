001f076c0b87a120de9941015f870833db1347fa
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-21 18:22:55 -0600

    feat: sort movies by their title

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import Movies from './components/Movies'
 import useMovies from './hooks/useMovies'
 import useSearch from './hooks/useSearch'
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [sort, setSort] = useState(false);
   const {title, setTitle, error} = useSearch();
-  const { movies: mappedMovies, loading, getMovies } = useMovies({title});
+  const { movies: mappedMovies, loading, getMovies } = useMovies({title, sort});
 
   const handleSumbit = (e) => {
     e.preventDefault();
     getMovies();
+  }
+
+  const handleSort = () => {
+    setSort(!sort);
   }
 
   const handleChange = (e) => {
@@ -24,6 +30,7 @@ function App() {
         <h1>Movie Search</h1>
         <form className='form' onSubmit={handleSumbit}>
           <input onChange={handleChange} style={{border: '1px solid transparent', borderColor: error ? 'red' : 'transparent'}} value={title} name='title' type="text" placeholder='Avengers, Star Wars, Interestellar, ...' />
+          <input type="checkbox" onChange={handleSort} checked={sort}/>
           {/* The last button of a form, is type submit */}
           <button>Search</button>
         </form>
